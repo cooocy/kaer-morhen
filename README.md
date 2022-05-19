b6009a4463dd928fcbfb67737996f845983b3e89
cooocy <cooocy@foxmail.com>
2022-05-19 19:22:36 +0800

    [style] 添加 README.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kaer Morhen
+
+> 名称出处
+
+**凯尔·莫罕**（[Kaer Morhen](http://witcher.gamepedia.com/Kaer_Morhen)）是上古语(精灵语)中的“卡尔阿木以拉罕(Caer a'Muirenhen)”，意为古海要塞(Old Sea Fortress)。因为城堡所在地曾是一片沧海，蓝山上有许多的海洋生物化石，于是有了古海要塞这个名字。
+
+凯尔·莫罕是一座难以抵达的山岳要塞，狩魔猎人公会总部数世纪以来就位于此处。坐落于[科德温](https://witcher.huijiwiki.com/wiki/科德温)境内的[蓝山](https://witcher.huijiwiki.com/wiki/蓝山)之中、葛温里屈河之上的凯尔·莫罕位于文明世界的边缘，被世人遗忘，很少有人知道通往堡垒的路。
+
+> 参考
+
+[凯尔·莫罕 - 猎魔人中文维基 | 獵魔士中文維基 | 巫师狩魔猎人攻略资料站 | The Witcher | Wiedźmin - 灰机wiki (huijiwiki.com)](https://witcher.huijiwiki.com/wiki/凯尔·莫罕)
--- oss_helper_bk.py
+++ oss_helper_bk.py
@@ -1,6 +1,6 @@
-import time
-import sys
 import oss2
+import sys
+import time
 
 
 def now():
@@ -18,7 +18,9 @@ def gen_oss_file_name(file_path):
 
 # args: [oss_helper.py, put, x, y, ...]
 args = sys.argv
-if len(args) < 3:
+args_len = len(args)
+
+if args_len < 3:
     print('args error')
     sys.exit(1)
 
@@ -28,12 +30,12 @@ bucket = oss2.Bucket(auth, 'oss-cn-shanghai.aliyuncs.com', 'z')
 action = args[1]
 
 if action == 'put':
-    for num in range(2, len(sys.argv)):
-        file_path = sys.argv[num]
+    for num in range(2, args_len):
+        file_path = args[num]
         res = bucket.put_object_from_file(gen_oss_file_name(file_path), file_path)
         print(res.resp.response.url.replace('http://', 'https://'))
 
 if action == 'del':
-    for num in range(2, len(sys.argv)):
-        res = bucket.delete_object(sys.argv[num])
+    for num in range(2, args_len):
+        res = bucket.delete_object(args[num])
         print(res.resp.response.ok)
